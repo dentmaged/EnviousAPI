e393e3837f250933261d854fe867cb1910550902
nverdierDev <nayamichaelv@gmail.com>
2015-04-19 02:20:29 -0700

    Added JavaDoc, MIT license, and #getLastPage method

--- src/main/java/com/nayaverdier/enviousapi/commandpages/CommandPages.java
+++ src/main/java/com/nayaverdier/enviousapi/commandpages/CommandPages.java
@@ -3,11 +3,35 @@ package com.nayaverdier.enviousapi.commandpages;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Copyright (c) 2015 nverdier
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to
+ * do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 public class CommandPages
 {
     private List<List<String>> pages;
-    private int                pageSize;
+    private int pageSize;
 
+    /**
+     * Creates a new CommandPages with specified page size and String...
+     *
+     * @param pageSize The size for each page to be
+     * @param lines    The lines that are initially added to the CommandPages
+     */
     public CommandPages(int pageSize, String... lines)
     {
         this.pageSize = pageSize;
@@ -15,11 +39,22 @@ public class CommandPages
         add(lines);
     }
 
+    /**
+     * Gets the amount of pages that are in the CommandPages
+     *
+     * @return The amount of pages in the CommandPages
+     */
     public int getTotalPages()
     {
         return pages.size();
     }
 
+    /**
+     * Gets the page at the specified index
+     *
+     * @param page The page number to get
+     * @return The page at the specified index
+     */
     public List<String> getPage(int page)
     {
         if (page >= pages.size())
@@ -27,6 +62,23 @@ public class CommandPages
         return pages.get(page);
     }
 
+    /**
+     * Gets the final page of the CommandPages
+     *
+     * @return The final page of the CommandPages
+     */
+    public List<String> getLastPage()
+    {
+        return getPage(getTotalPages() - 1);
+    }
+
+    /**
+     * Gets the String at the specified page and line numbers
+     *
+     * @param pageNumber The page to retrieve the String from
+     * @param lineNumber The line to retrieve the String from
+     * @return The String at specified page and line
+     */
     public String getLine(int pageNumber, int lineNumber)
     {
         if (pageNumber >= pages.size())
@@ -37,6 +89,11 @@ public class CommandPages
         return page.get(lineNumber);
     }
 
+    /**
+     * Adds a String... to the CommandPages
+     *
+     * @param lines The lines to add
+     */
     public void add(String... lines)
     {
         for (String s : lines)
